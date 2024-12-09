c5a5187e177768db998135a8cab03eeab10d119f
NN125 <nicolas.nolden@student.swic.edu>
2024-12-08 18:16:19 -0600

    Refactor button.js

--- src/components/button.js
+++ src/components/button.js
@@ -1,3 +1,15 @@
-export default function Button(type = "button", addlClasses = "", text) {
-  return `<button type="${type}" class="button ${addlClasses} rounded bg-orange-300 hover:bg-orange-500 transition text-white p-2">${text}</button>`;
+export default function Button({
+  // Default values for the button
+  type = "button",
+  additionalClasses = "",
+  text,
+}) {
+  return `
+    <button 
+      type="${type}" 
+      class="rounded shadow transition-colors ${additionalClasses}"
+    >
+      ${text}
+    </button>
+  `;
 }
