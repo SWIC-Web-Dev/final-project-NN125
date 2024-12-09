c2f50589d536ede70fa9529adbe9d8f6868e3fa4
NN125 <nicolas.nolden@student.swic.edu>
2024-12-08 18:16:24 -0600

    Refactor input.js

--- src/components/input.js
+++ src/components/input.js
@@ -1,5 +1,11 @@
 export default function Input({ id, placeholder, additionalClasses = "" }) {
   return `
-    <input id="${id}" class="input ${additionalClasses}" type="text" placeholder="${placeholder}" />
+    <input 
+      id="${id}" 
+      class="w-full px-3 py-2 border rounded shadow-sm ${additionalClasses}" 
+      type="text" 
+      placeholder="${placeholder}"
+      required
+    />
   `;
 }
