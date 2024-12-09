68b57a50f26f3a643879deab55cfc8db958ad402
NN125 <nicolas.nolden@student.swic.edu>
2024-12-08 19:00:22 -0600

    Fix background color not covering the entire heading

--- src/components/items-table/index.js
+++ src/components/items-table/index.js
@@ -2,8 +2,8 @@ import ItemRow from "./item-row";
 
 export default function ItemsTable(items) {
   return `
-    <table class="w-full mt-4 border-collapse border">
-      <thead class="bg-gray-100">
+    <table class="w-full mt-4 border-collapse border bg-gray-100">
+      <thead>
         <tr class="*:p-2 *:text-left">
           <th>ID</th>
           <th>Name</th>
