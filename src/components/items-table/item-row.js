5d91b3414cd381a17cf8adff75ec1c24dddfca5a
NN125 <nicolas.nolden@student.swic.edu>
2024-12-08 18:31:19 -0600

    Rename data-dish to data-item to allow the delete button to work

--- src/components/items-table/item-row.js
+++ src/components/items-table/item-row.js
@@ -9,14 +9,14 @@ export default function ItemRow({
   stock,
 }) {
   return `
-    <tr class="border-b *:p-2" data-dish=${id}>
+    <tr class="border-b *:p-2" data-item=${id}>
       <td>${id}</td>
       <td>${name}</td>
       <td>${description}</td>
       <td>${price}</td>
       <td>${category}</td>
       <td>${stock}</td>
-      <td>${Button({ text: "Delete", additionalClasses: "bg-red-500 rounded text-white px-2 py-1" })}</td>
+      <td>${Button({ type: "button", text: "Delete", additionalClasses: "bg-red-500 rounded text-white px-2 py-1 delete-button" })}</td>
     </tr>
   `;
 }
